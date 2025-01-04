a9dd0695b3e2dbf493f8795d89649a854b323cb4
Sunnesha123 <sunneshamudi2015@gmail.com>
2025-01-05 01:10:54 +0530

    concatination

---
+++ concatination.java
@@ -0,0 +1,14 @@
+/**
+ * concatination
+ */
+public class concatination {
+
+     String concat (String s)
+    {
+        return(s+"@gmail.com");
+    }
+    public static void main(String[] args) {
+        concatination obj = new concatination();
+        System.out.println( obj.concat("sunnesha"));
+    }
+}
