874b3ae40c989ba8a2fa5b0bedb4921dc31fdd47
Sunnesha123 <sunneshamudi2015@gmail.com>
2025-01-19 11:25:37 +0530

    Table

---
+++ Table.java
@@ -0,0 +1,19 @@
+public class Table 
+{
+        void tableCalc()
+         {
+          for(int i=1;i<=10;i++)
+          {
+
+          }
+         }
+
+      
+         
+        public static void main(String[] args)
+         {
+            Table a=  new Table();
+          System.out.println(a.tableCalc());
+      
+         }
+}
