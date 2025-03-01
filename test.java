6a98368c79c76470300f410a039813864992bedc
Sunnesha123 <sunneshamudi2015@gmail.com>
2025-03-02 00:51:18 +0530

    add

---
+++ test.java
@@ -0,0 +1,16 @@
+public class test 
+{
+        int sum( int a ,int b)
+         {
+          int c = a+b;
+          return c;
+         }
+      
+         
+        public static void main(String[] args)
+         {
+          test a=  new test();
+          System.out.println(a.sum(2, 20));
+      
+         }
+}
